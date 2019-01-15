253cd35e5758f62df20cecd05ef48108a80d227c
student <student@localhost>
2019-01-15 17:36:11 +0400

    Codecov added.

--- src/test/java/ru/itpark/CreditServiceTest.java
+++ src/test/java/ru/itpark/CreditServiceTest.java
@@ -1,5 +1,6 @@
 package ru.itpark;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,36 +8,54 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreditServiceTest {
 
     @Test
+    @DisplayName("Credit 50_000")
     void creditCalculator() {
         {
             CreditService service = new CreditService();
-            double creditCalculator = service.creditCalculator(50_000,22,12);
+            double creditCalculator = service.creditCalculator(50_000, 22, 12);
 
-            assertEquals(56_156,creditCalculator);
+            assertEquals(56_156, creditCalculator);
         }
+    }
+    @Test
+    @DisplayName("Creditdata 0")
+    void creditCalculatorWithZeroCreditdata() {
         {
             CreditService service = new CreditService();
-            double creditCalculator = service.creditCalculator(0,22,12);
+            double creditCalculator = service.creditCalculator(50_000, 22, 0);
 
-            assertEquals(0,creditCalculator);
+            assertEquals(0, creditCalculator);
         }
+    }
+
+    @Test
+    @DisplayName("IntrestRate 0")
+    void creditCalculatorWithZeroIntrestRate() {
         {
             CreditService service = new CreditService();
-            double creditCalculator = service.creditCalculator(50_000,22,0);
+            double creditCalculator = service.creditCalculator(50_000, 0, 12);
 
-            assertEquals(0,creditCalculator);
+            assertEquals(0, creditCalculator);
         }
+    }
+    @Test
+    @DisplayName("Credit 0")
+    void creditCalculatorWithZeroSumm() {
         {
             CreditService service = new CreditService();
-            double creditCalculator = service.creditCalculator(50_000,0,12);
+            double creditCalculator = service.creditCalculator(0, 22, 12);
 
-            assertEquals(0,creditCalculator);
+            assertEquals(0, creditCalculator);
         }
+    }
+    @Test
+    @DisplayName("Credit 10% IntrestRate ")
+    void creditCalculatorWithIntrestRate() {
         {
             CreditService service = new CreditService();
-            double creditCalculator = service.creditCalculator(50_000,10,12);
+            double creditCalculator = service.creditCalculator(50_000, 10, 12);
 
-            assertEquals(52_749,creditCalculator);
+            assertEquals(52_749, creditCalculator);
         }
     }
 }
